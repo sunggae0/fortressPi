406242939a942d01584145392d92d0f0c4f6fa54
sunggae0 <mesjp1130@gmail.com>
2025-12-19 02:15:00 +0900

    render

---
+++ src/render.c
@@ -0,0 +1,77 @@
+#include <bcm2835.h>
+#include <stdio.h>
+
+#include "st7789.h"
+
+
+void writeCommand(uint8_t cmd) {
+    bcm2835_gpio_clr(TFT_DC);
+    bcm2835_spi_transfer(cmd);
+}
+
+void writeData(uint8_t data) {
+    bcm2835_gpio_set(TFT_DC);
+    bcm2835_spi_transfer(data);
+}
+
+void st7789_init() {
+    bcm2835_spi_begin();
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_8);
+    
+    // Hardware reset
+    bcm2835_gpio_clr(TFT_RST);
+    delay(100);
+    bcm2835_gpio_set(TFT_RST);
+    delay(100);
+
+    writeCommand(ST7789_SWRESET); // Software reset
+    delay(150);
+
+    writeCommand(ST7789_SLPOUT);  // Sleep out
+    delay(500);
+
+    writeCommand(ST7789_COLMOD);  // Set color mode
+    writeData(0x55);              // 16-bit color
+    delay(10);
+
+    writeCommand(ST7789_MADCTL);
+    writeData(0x00);              // Normal display
+    
+    writeCommand(0x21);
+
+
+    writeCommand(ST7789_CASET);
+    writeData(0x00); writeData(0x00); // XSTART = 0
+    writeData(ST7789_TFTWIDTH >> 8); writeData(ST7789_TFTWIDTH & 0xFF); // XEND
+
+    writeCommand(ST7789_RASET);
+    writeData(0x00); writeData(0x00); // YSTART = 0
+    writeData(ST7789_TFTHEIGHT >> 8); writeData(ST7789_TFTHEIGHT & 0xFF); // YEND
+
+    writeCommand(ST7789_DISPON);  // Display on
+    delay(100);
+}
+
+
+
+void render_screen() {
+    writeCommand(ST7789_RASET);
+    writeData(0); writeData(0);
+    writeData(ST7789_TFTHEIGHT >> 8); writeData(ST7789_TFTHEIGHT & 0xFF);
+    
+    writeCommand(ST7789_CASET);
+    writeData(0); writeData(0);
+    writeData(ST7789_TFTWIDTH >> 8); writeData(ST7789_TFTWIDTH & 0xFF);
+    
+    writeCommand(ST7789_RAMWR);
+    bcm2835_gpio_set(TFT_DC);
+    
+    int i, j;
+    for (i = 0; i < 240; i++) {
+        for (j = 0; j < 240; j++) {
+            uint8_t hi = screen[i][j] >> 8, lo = screen[i][j] & 0xFF;
+            bcm2835_spi_transfer(hi);
+            bcm2835_spi_transfer(lo);
+        }
+    }
+}
